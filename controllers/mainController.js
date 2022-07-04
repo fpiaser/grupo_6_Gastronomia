03f0933b05bb1409fc23c1f9a78f50c17df0fac9
vperez864 <vperez864@gmail.com>
2022-07-04 18:13:53 -0500

    Actualziación de todos las vistas con ejs

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -3,36 +3,32 @@ const path = require('path');
 const mainController = {
     home:(req, res)=>{
         res.sendFile(
-            path.join(
-                __dirname, '../views/home.html'
-            )
+            //path.join(__dirname, '../views/home.html')
+            res.render('../views/home',{})
         )
     },
     productDetail:(req, res)=>{
         res.sendFile(
-            path.join(
-                __dirname, '../views/products/productDetail.html'
-            )
+            //path.join(__dirname, '../views/products/productDetail.html')
+            res.render('../views/products/productDetail',{})
         )
     },
     login:(req, res)=>{
         res.sendFile(
-            path.join(
-                __dirname, '../views/users/login.html'
-            )
+            //path.join(__dirname, '../views/users/login.html')
+            res.render('../views/users/login',{})
         )
     },
     register:(req, res)=>{
         res.sendFile(
-            path.join(
-                __dirname, '../views/users/register.html'
-            )
+            //path.join(__dirname, '../views/users/register.html')
+            res.render('../views/users/register',{})
         )
     },
     ProductCart: (req, res) => {
         res.sendFile(
-            path.join(__dirname,'../views/products/productCart.html')
-            //res.render("productCart",{})
+            //path.join(__dirname,'../views/products/productCart.html')
+            res.render('../views/products/productCart',{})
         );
     }
 }
