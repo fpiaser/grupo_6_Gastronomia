460df47a1a7a239e977b7c6bc7b99ac6e8940887
EstephanyGonzalez12 <estefanygonzalez999@gmail.com>
2022-08-09 11:22:39 -0500

    Procesamiento de post para registro de usuarios

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,10 @@
+const fs = require('fs');
+const path = require('path');
+const registerListPath = path.resolve(__dirname, '../data/user.json');
+const registerList = JSON.parse(fs.readFileSync(registerListPath, 'utf8'));
+const { v4: uuidv4 } = require('uuid');
+
+
 const userController = {
     login: (req, res)=>{
         res.render('../views/users/login',{
@@ -13,6 +20,28 @@ const userController = {
         })
         
     },
+//POST QUE RECIBE Y PROCESA REGISTROS
+    saveRegister:(req, res)=>{
+        let register = req.body;
+        let image = req.file;
+       //  let images = req.files;
+
+        if (image) {
+           product.imagen = image.filename;
+       } 
+       
+       // else if (images) {
+       //     product.imagen = images.map(image => image.filename);
+       // }
+
+        register.id = uuidv4();
+
+       registerList.push(register);
+       
+
+       fs.writeFileSync(registerListPath, JSON.stringify(registerList, null, 2));
+        res.redirect('/');
+    }
 }
 
 module.exports = userController;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const uploadFile=  require('../middlewares/multer');
 
 router.get('/registro', userController.register);
+router.post('/', uploadFile.single('imagen'), userController.saveRegister);
+
 router.get('/login', userController.login);
 
 module.exports = router;
