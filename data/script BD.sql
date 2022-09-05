4283c78c08165ae18ed67ff057b373d6ebbd420e
vperez864 <vperez864@gmail.com>
2022-09-05 10:05:54 -0500

    Se adjunta diagrama de base de datos y script de base de datos

---
+++ data/script BD.sql
@@ -0,0 +1,59 @@
+CREATE DATABASE `GastVegana` ;
+
+
+CREATE TABLE `categoria` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb
+
+
+CREATE TABLE `unidad_medida` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+)  ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `apellido` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `image` varchar(100) NOT NULL,
+  `Admin` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- products definition
+
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `descripcion` varchar(1000) NOT NULL,
+  `uom` int(11) NOT NULL,
+  `idcategoria` int(11) NOT NULL,
+  `precio` int(11) NOT NULL,
+  `image` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idcategoria` (`idcategoria`),
+  KEY `uom` (`uom`),
+  CONSTRAINT `products_ibfk_1` FOREIGN KEY (`idcategoria`) REFERENCES `categoria` (`id`),
+  CONSTRAINT `products_ibfk_2` FOREIGN KEY (`uom`) REFERENCES `unidad_medida` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+CREATE TABLE `shopping_cart` (
+  `id` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `id_product` int(11) NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `fecha_compra` date NOT NULL,
+  `valor_unitario` int(11) NOT NULL,
+  `valor_tota` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `shopping_cart_FK` (`id_product`),
+  KEY `shopping_cart_FK_1` (`id_user`),
+  CONSTRAINT `shopping_cart_FK` FOREIGN KEY (`id_product`) REFERENCES `products` (`id`),
+  CONSTRAINT `shopping_cart_FK_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
