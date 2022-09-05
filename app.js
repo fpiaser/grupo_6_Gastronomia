5a6a5f31e7531761ba216a31e50a4368801490c3
Andreiev Valencia <andreivalse@gmail.com>
2022-09-05 18:27:27 -0500

    pruebas sequelize

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const path = require('path');
 const app = express();
@@ -5,17 +6,30 @@ const methodOverride = require('method-override');
 var session = require('express-session');
 const cookieParser = require('cookie-parser');
 const bcrypt = require('bcrypt');
+const db = require('./config/db.js') 
 
-const PORT= process.env.PORT || 3000
+const PORT= process.env.PORT || 3100
 const mainRoutes = require('./routes/mainRoutes');
 const userRoutes = require('./routes/userRoutes');
 const productRoutes = require('./routes/productRoutes');
 const logMiddleware = require('./middlewares/logMiddleware');
 
+//Conexion a la base de datos
+db.authenticate()
+.then(()=>{
+    console.log('Conexión a la Base de datos') 
+})
+.cath(error =>{
+    console.log(error)
+})
+    
 
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+
+
 
 app.use(session({secret: 'secreto'}));
 app.use(logMiddleware);
