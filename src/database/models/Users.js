d059449f1ade1165696688fd55445c5954dde8ff
Andreiev Valencia <andreivalse@gmail.com>
2022-09-20 17:57:25 -0500

    se agrega tableName

--- src/database/models/Users.js
+++ src/database/models/Users.js
@@ -34,6 +34,7 @@ module.exports = (sequelize, dataTypes) => {
         
     };
     let config = {
+        tableName: 'users',
         timestamps: false
         //createdAt: 'created_at',
         //updatedAt: 'updated_at',
