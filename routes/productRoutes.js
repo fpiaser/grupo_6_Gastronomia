413f55b52a0778e16f74ca4b3ae055df1d92e4cf
vperez864 <vperez864@gmail.com>
2022-09-26 12:36:22 -0500

    Validaciones BackEnd User y Products

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -3,7 +3,36 @@ const router = express.Router();
 const productController = require('../controllers/productController');
 const uploadFile=  require('../middlewares/multer');
 const authMiddleware = require('../middlewares/authMiddleware');
-const guestMiddleware = require('../middlewares/guestMiddleware')
+const guestMiddleware = require('../middlewares/guestMiddleware');
+const { body } = require('express-validator');
+
+
+const validationResult=[
+    body('nombre')
+    .notEmpty().withMessage('Debe ingresar un nombre')
+    .isLength({min:5}).withMessage('El nombre debe tener al menos 5 caracteres'),
+    body('descripcion')
+    .notEmpty().withMessage('Debe ingresar una descripción')
+    .isLength({min:20}).withMessage('El nombre debe tener al menos 20 caracteres'),
+    body('precio')
+    .notEmpty().withMessage('Debe ingresar el precio'),
+    body('uom')
+    .custom((value, { req }) => {
+        if (req.value === 0) {
+            return false;
+        }
+        return true;
+    }
+).withMessage('Debe seleccionar la Unidad de medida'),
+    body('imagen')
+    .custom((value, { req }) => {
+        if (req.files.length === 0) {
+            return false;
+        }
+        return true;
+    }
+).withMessage('Debe subir una imagen')
+];
 
 //Ruta productos
 // ACCESIBLE POR CUALQUIERA
@@ -17,12 +46,12 @@ router.get('/carrito', productController.productCart);
 //Ruta crear producto
 // ACCESIBLE SOLO CON LOGIN (sino redirige al login)
 router.get('/nuevoProducto',authMiddleware, productController.newProduct);
-router.post('/',authMiddleware, uploadFile.single('imagen'), productController.storeProduct);
+router.post('/',authMiddleware, uploadFile.single('imagen'),validationResult, productController.storeProduct);
 
 //Ruta editar Producto
 // ACCESIBLE SOLO CON LOGIN (sino redirige al login)
 router.get('/modificarProducto/:id',authMiddleware, productController.modProduct);
-router.put('/:id',authMiddleware, productController.updateProduct);
+router.put('/:id',authMiddleware,validationResult, productController.updateProduct);
 
 //Ruta de eliminar un producto
 // ACCESIBLE SOLO CON LOGIN (sino redirige al login)
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,18 +2,43 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 const uploadFile=  require('../middlewares/multer');
-const {body} = require('express-validator')
+const { body } = require('express-validator');
 const authMiddleware = require('../middlewares/authMiddleware');
-const guestMiddleware = require('../middlewares/guestMiddleware')
+const guestMiddleware = require('../middlewares/guestMiddleware');
+
+const validationResult=[
+    body('nombre')
+    .notEmpty().withMessage('Debe ingresar un nombre')
+    .isLength({min:2}).withMessage('El nombre debe tener al menos 2 caracteres'),
+    
+    body('apellido')
+    .notEmpty().withMessage('Debe ingresar su apellido')
+    .isLength({min:2}).withMessage('El apellido debe tener al menos 2 caracteres'),
+    body('email')
+    .notEmpty().withMessage('Debe ingresar un Email')
+    .isEmail().withMessage('Debe ingresa un Email válido'),
+    body('password')
+    .notEmpty().withMessage('Debe ingresar una contraseña')
+    .isLength({min:8}).withMessage('Su contraseña debe tener al menos 8 caracteres'),
+    body('imagen')
+        .custom((value, { req }) => {
+            if (req.files.length === 0) {
+                return false;
+            }
+            return true;
+        }
+    ).withMessage('Debe subir una imagen')
+]; 
+
 
 //Ruta listado usuarios
-// ACCESIBLE SOLO CON LOGIN (sino redirige al login)
+// ACCESIBLE SOLO CON LOGIN (sino redirige al login)s
 router.get('/',authMiddleware, userController.user);
 
 //Ruta crear usuario
 // ACCESIBLE SOLO SIN LOGIN (sino redirige al perfil)
 router.get('/registro',guestMiddleware, userController.register);
-router.post('/',guestMiddleware, uploadFile.single('imagen'), userController.create);
+router.post('/',guestMiddleware, uploadFile.single('imagen'),validationResult , userController.create);
 
 //Ruta login
 // ACCESIBLE SOLO SIN LOGIN (sino redirige al perfil)
@@ -23,7 +48,7 @@ router.post('/login',guestMiddleware, userController.processLogin);
 //Ruta editar Usuario
 // ACCESIBLE SOLO CON LOGIN (sino redirige al login)
 router.get('/editarUsuario/:id',authMiddleware, userController.editUser);
-router.put('/:id',authMiddleware, userController.update);
+router.put('/:id',authMiddleware,validationResult, userController.update);
 
 //Ruta detalle usuario
 // ACCESIBLE SOLO CON LOGIN (sino redirige al login)
