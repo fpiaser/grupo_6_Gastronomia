8dc7db551cf419cc7832428f991056f962a55b5e
Andreiev Valencia <andreivalse@gmail.com>
2022-10-17 16:03:31 -0500

    Cambio de remembesme

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,9 +1,11 @@
 function authMiddleware(req, res, next) {
+    //if (!req.session.user && req.cookies.user) {
     if (req.session.user != undefined) {
+        req.session.user = req.cookies.user
         next();
-    } else {
+    }  else {
         res.send('Esta pagina es solo para usuarios')
-    }
+    } 
 }
 
 module.exports = authMiddleware;
